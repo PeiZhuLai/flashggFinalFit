12a4c541003399e877495efec60f4631dcec36bc
Pei-Zhu Lai <ht910155@gmail.com>
2026-04-04 10:27:53 +0200

    check signal yield shell scripts

---
+++ shellScripts/sig_sys/A_runjob_check_signal_yield.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+pwd
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+CMSSW_TOP="/afs/cern.ch/work/p/pelai/HZa/flashgg_run3/CMSSW_14_1_0_pre4"
+cd "${CMSSW_TOP}/src"
+cmsenv
+
+dir_sig="/afs/cern.ch/work/p/pelai/HZa/flashgg_run3/CMSSW_14_1_0_pre4/src/flashggFinalFit/Signal"
+source /afs/cern.ch/work/p/pelai/HZa/flashgg_run3/CMSSW_14_1_0_pre4/src/flashggFinalFit/setup.sh
+export PYTHONPATH=$PYTHONPATH:/afs/cern.ch/work/p/pelai/HZa/flashgg_run3/CMSSW_14_1_0_pre4/src/flashggFinalFit/tools:/afs/cern.ch/work/p/pelai/HZa/flashgg_run3/CMSSW_14_1_0_pre4/src/flashggFinalFit/Signal/tools
+
+# ------------- Open for Condor -------------
+# python3 $dir_sig/RunPlotter.py --mass_ALP $1 --years $2 --channel $3
+
+# ------------- Open for Dry Run -------------
+# python3 $dir_sig/RunPlotter.py --mass_ALP 5 --years 2022preEE --channel ele
+
+# ------------- Open for Local Run -------------
+python3 $dir_sig/scripts/audit_signal_yields.py \
+  --masses 20 \
+  --years 2024 \
+  --channels ele \
+  --csv audit_m20_2024_ele.csv
