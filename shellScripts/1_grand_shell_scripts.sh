3dcb5bbc69817912edc7dde5f29402a6201bd1ab
Pei-Zhu Lai <ht910155@gmail.com>
2026-05-02 17:56:39 +0200

    bkg output

--- shellScripts/1_grand_shell_scripts.sh
+++ shellScripts/1_grand_shell_scripts.sh
@@ -10,16 +10,22 @@ baseDir=/afs/cern.ch/work/p/pelai/HZa/flashgg_run3/CMSSW_14_1_0_pre4/src/flashgg
 # sh $baseDir/Trees2WS/run_tree2ws.sh
 
 ### ----------- Background 
-cd $baseDir/shellScripts
-sh $baseDir/shellScripts/bkg/fit_bkg.sh
+# cd $baseDir/shellScripts
+# sh $baseDir/shellScripts/bkg/fit_bkg.sh
+### ----------- Background Condor
+# bash $baseDir/shellScripts/bkg/Condor/subjob_bkg.sh
+# bash $baseDir/shellScripts/bkg/Condor/collect_bkg_results.sh
+### ----------- Background Condor run along 
+# bash bkg/Condor/subjob_bkg.sh
+# bash bkg/Condor/collect_bkg_results.sh
 
 ### ----------- Signal
-cd $baseDir/shellScripts
-sh $baseDir/shellScripts/sig_sys/1_runjob_sig_fTest.sh
-sh $baseDir/shellScripts/sig_sys/2_runjob_sig_calcPhotonSyst.sh
-sh $baseDir/shellScripts/sig_sys/3_runjob_sig_signalFit.sh
-sh $baseDir/shellScripts/sig_sys/4_runjob_sig_RunPlotter.sh
-sh $baseDir/shellScripts/sig_sys/5_runjob_sig_plotEffSigma.sh
+# cd $baseDir/shellScripts
+# sh $baseDir/shellScripts/sig_sys/1_runjob_sig_fTest.sh
+# sh $baseDir/shellScripts/sig_sys/2_runjob_sig_calcPhotonSyst.sh
+# sh $baseDir/shellScripts/sig_sys/3_runjob_sig_signalFit.sh
+# sh $baseDir/shellScripts/sig_sys/4_runjob_sig_RunPlotter.sh
+# sh $baseDir/shellScripts/sig_sys/5_runjob_sig_plotEffSigma.sh
 
 ### ----------- Datacard
 cd $baseDir/Datacard
@@ -27,20 +33,20 @@ sh 1_runjob_gen_datacard_makeYields.sh
 sh 2_runjob_gen_datacard_makeDatacard.sh
 sh 3_rysn_datacard.sh
 
-### ----------- Combine Limits
-cd $baseDir/Combine
-sh 1_makeLimits.sh
+# ### ----------- Combine Limits
+# cd $baseDir/Combine
+# sh 1_makeLimits.sh
 
-### ----------- Plot Limts
-cd $baseDir/Plots
-sh 1_runLimitsPlot.sh
+# ### ----------- Plot Limts
+# cd $baseDir/Plots
+# sh 1_runLimitsPlot.sh
 
-### ----------- Impact Plot
-cd $baseDir/Combine
-sh 2_expectedImpact.sh
+# ### ----------- Impact Plot
+# cd $baseDir/Combine
+# sh 2_expectedImpact.sh
 
-### ----------- Bias Study
-cd $baseDir/Combine/Checks/Bias_nominal
-sh 1_bias_study.sh
+# ### ----------- Bias Study
+# cd $baseDir/Combine/Checks/Bias_nominal
+# sh 1_bias_study.sh
 
 cd $baseDir/shellScripts
